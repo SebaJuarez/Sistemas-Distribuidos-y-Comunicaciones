2cd97a285a8460654185801dee4be7bed522d1cc
SebaJuarez <113382513+SebaJuarez@users.noreply.github.com>
2025-04-03 14:53:17 -0300

    feat: implementación de feign client para la tarea remota

---
+++ Servidor/src/main/java/SistemaDistribuido/Servidor/feignClient/TareaFeignClient.java
@@ -0,0 +1,20 @@
+package SistemaDistribuido.Servidor.feignClient;
+
+import SistemaDistribuido.Servidor.dtos.ParametroTareaDTO;
+import SistemaDistribuido.Servidor.dtos.RespuestaTareaDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "tareaClient", url = "${container.url}")
+public interface TareaFeignClient {
+
+    @PostMapping(value = "${container.endpoint.ejecutarTarea}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<RespuestaTareaDTO> ejecutarTarea(@RequestBody ParametroTareaDTO parametroTarea);
+
+    @GetMapping(value = "${container.endpoint.health}")
+    ResponseEntity<String> healthCheck();
+}
